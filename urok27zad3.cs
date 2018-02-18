092d0afa44a1eaee392955257e60c3ef10ac4303
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-02-18 13:24:56 +0200

    Add files via upload

---
+++ urok27zad3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace urok27zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> goods = new Dictionary<string, int>();
+            int amount = 0;
+            string food = Console.ReadLine();
+            int i = 0;
+            while (!food.Equals("stop"))
+            {
+                if (i % 2 == 0)
+                {
+                    amount = int.Parse(Console.ReadLine());
+                    goods.Add(food, amount);
+                    i += 2;
+                }
+                else
+                {
+                    i++;
+                }
+                food = Console.ReadLine();
+            }
+            foreach (var good in goods)
+            {
+                Console.WriteLine("{0} --> {1}", good.Key, good.Value);
+            }
+        }
+    }
+}
