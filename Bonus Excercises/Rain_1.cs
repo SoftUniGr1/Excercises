45d6b9ab1b4921fc72d082b6e7611ea319eddc23
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-02-22 21:19:43 +0200

    Add files via upload

---
+++ Bonus Excercises/Rain_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rain_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int regions = int.Parse(Console.ReadLine());
+            double sumRain = 0;
+            double density = double.Parse(Console.ReadLine());
+            Dictionary<int, int> rainRegion = new Dictionary<int, int>();
+            List<double> rainInTotal = new List<double>();
+            if (density>0 && density <= 9)
+            {
+                for (int i = 0; i<regions; i++)
+                {
+                    int[] rainTotal = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                    rainRegion.Add(rainTotal[0], rainTotal[1]);
+                }
+                foreach (var rain in rainRegion)
+                {
+                    int intRain = rain.Key;
+                    rainInTotal.Add((intRain * 1.0) / (rainRegion[rain.Key]*1.0));
+                    sumRain += (intRain * 1.0) / (rainRegion[rain.Key] * 1.0);
+                }
+                Console.WriteLine("{0:f3}", sumRain / density);
+            }
+        }
+    }
+}
