669d8df51645167d330131453afe674f73d1c752
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-02-24 09:13:58 +0200

    Add files via upload

---
+++ Bonus Excercises/Phoenix_1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoenix_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double[] sum = new double[number];
+            for (int i = 0; i<number; i++)
+            {
+                int bodyLength = int.Parse(Console.ReadLine());
+                double bodyWidth = double.Parse(Console.ReadLine());
+                int wingLength = int.Parse(Console.ReadLine());
+                sum[i] = (bodyLength * bodyLength) * (bodyWidth + (2 * wingLength));
+            }
+            for (int i = 0; i<number; i++)
+            {
+                Console.WriteLine(sum[i]);
+            }
+        }
+    }
+}
