04b12d6edc0b8d5af499df1c386b68fbca2a35d1
Vasilena Kolarova <35624769+VasilenaKolarova@users.noreply.github.com>
2018-02-24 13:39:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] array = input.ToCharArray();
+            char[] reversedArray = new char[array.Length];
+            int position = 0;
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                reversedArray[position] = array[i];
+                position++;
+            }
+            bool rezult = true;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != reversedArray[i])
+                {
+                    rezult = false;
+                }
+            }
+            if (rezult == true)
+            {
+                Console.WriteLine("True");
+
+            }
+            else
+                Console.WriteLine("False");
+        }
+    }
+}
